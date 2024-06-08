4219bd7c2641d09fc02f973fa2e5635f6b71ced5
Raza Sayed <raza.sayed@gmail.com>
2024-06-08 21:50:23 +0530

    Using OpenAI llm

--- query_data.py
+++ query_data.py
@@ -5,6 +5,11 @@ from langchain_community.llms.ollama import Ollama
 from get_embedding_function import get_embedding_function
 from flask import Flask, request, jsonify
 
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+
+llm_name = "gpt-3.5-turbo"
+
 CHROMA_PATH = "chroma"
 
 PROMPT_TEMPLATE = """
@@ -43,17 +48,28 @@ def query_rag(query_text: str):
     # Search the DB.
     results = db.similarity_search_with_score(query_text, k=5)
 
-    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=query_text)
-    # print(prompt)
+    llm = ChatOpenAI(model_name=llm_name, temperature=0)
 
-    model = Ollama(model="mistral")
-    response_text = model.invoke(prompt)
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=db.as_retriever()
+    )
 
-    sources = [doc.metadata.get("id", None) for doc, _score in results]
-    formatted_response = f"Response: {response_text}\nSources: {sources}"
-    print(formatted_response)
+    result = qa_chain({"query": query_text})
+
+    response_text = result["result"]
+
+    # context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    # prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    # prompt = prompt_template.format(context=context_text, question=query_text)
+    # # print(prompt)
+    #
+    # model = Ollama(model="mistral")
+    # response_text = model.invoke(prompt)
+    #
+    # sources = [doc.metadata.get("id", None) for doc, _score in results]
+    # formatted_response = f"Response: {response_text}\nSources: {sources}"
+    # print(formatted_response)
     return response_text
 
 
